d70bebe5a4ff27753b4e4cbcf9be022731f64223
Daniel Pope <mauve@mauveweb.co.uk>
2013-01-22 22:02:08 +0000

    Optimise, by determining if the remaining ships could not fit in the space available

--- battleships.py
+++ battleships.py
@@ -1,8 +1,14 @@
 GRID_SIZE = 6
 grid = {}
+ships = {1:1, 2:2, 3:1, 4:1}
 
 possible_moves = [(x, y) for x in range(1, GRID_SIZE + 1) for y in range(1, GRID_SIZE + 1) if x % 2 ^ y % 2]
 possible_moves += [(x, y) for x in range(1, GRID_SIZE + 1) for y in range(1, GRID_SIZE + 1) if not (x % 2 ^ y % 2)]
+
+def min_length():
+    for i in range(1, 5):
+        if ships[i]:
+            return i
 
 def pick_random():
     return possible_moves.pop()
@@ -53,7 +59,32 @@ class strafe(object):
             self.directions.pop(0)
         except IndexError:
             strategy = pick_random
+            return
         self.pos = self.first_hit
+        dir = self.directions[0]
+        x, y = dir
+        odir = -x, -y
+        len = (
+            self.consider_direction(dir) +
+            1 +
+            self.consider_direction(odir)
+        )
+        if len < min_length():
+            self.assign_new_direction()
+
+    def consider_direction(self, dir):
+        moved = 0
+        pos = self.first_hit
+        while True:
+            x, y = pos 
+            dx, dy = dir
+            pos = (x + dx, y + dy)
+            if not coord_in_grid(*pos):
+                return moved
+            if grid.get(pos) == 'm':
+                return moved
+            moved += 1
+
 
 def print_location(x, y):
     '''
@@ -85,6 +116,18 @@ if __name__ == '__main__':
             if not isinstance(strategy, strafe):
                 strategy = strafe(guess)
         elif outcome == 's':
+            while True:
+                l = raw_input('what length of ship?')
+                try:
+                    l = int(l.strip())
+                except ValueError:
+                    print 'bad length'
+                else:
+                    if not ships.get(l, 0):
+                        print 'No ships that length'
+                    else:
+                        break
+            ships[l] -= 1
             strategy = pick_random
         elif outcome == 'm':
             pass
