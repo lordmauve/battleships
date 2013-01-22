066dab9640465c392627ed784acdbbe854fe9248
Daniel Pope <mauve@mauveweb.co.uk>
2013-01-22 21:44:37 +0000

    Debugged; optimised random strategy

--- battleships.py
+++ battleships.py
@@ -1,19 +1,19 @@
-import random
-
+GRID_SIZE = 6
 grid = {}
 
+possible_moves = [(x, y) for x in range(1, GRID_SIZE + 1) for y in range(1, GRID_SIZE + 1) if x % 2 ^ y % 2]
+possible_moves += [(x, y) for x in range(1, GRID_SIZE + 1) for y in range(1, GRID_SIZE + 1) if not (x % 2 ^ y % 2)]
+
 def pick_random():
-    while True:
-        x = random.randint(1, 11)
-        y = random.randint(1, 11)
-        if (x, y) not in grid:
-            return x, y
+    return possible_moves.pop()
 
 def coord_in_grid(x, y):
-    return 0 < x < 11 and 0 < y < 11
+    return 0 < x <= GRID_SIZE and 0 < y <= GRID_SIZE
 
 class strafe(object):
     def __init__(self, first_hit):
+        print "Strafing from",
+        print_location(*first_hit)
         self.first_hit = first_hit
 
         self.directions = [
@@ -26,9 +26,12 @@ class strafe(object):
         
     def __call__(self):
         global strategy
-        if outcome is 'm':
+        if outcome == 'm':
             self.assign_new_direction()
+        print "First hit",
+        print_location(*self.first_hit)
         while self.directions:
+            print "Direction", self.directions[0]
             x, y = self.pos
             dx, dy = self.directions[0]
             self.pos = (x + dx, y + dy)
@@ -42,6 +45,7 @@ class strafe(object):
                     break
             else:
                 self.assign_new_direction()
+        possible_moves.remove(self.pos)
         return self.pos
 
     def assign_new_direction(self):
@@ -78,7 +82,7 @@ if __name__ == '__main__':
                 break
         grid[guess] = 'h' if outcome in 'hs' else 'm'
         if outcome == 'h':
-            if strategy is not strafe:
+            if not isinstance(strategy, strafe):
                 strategy = strafe(guess)
         elif outcome == 's':
             strategy = pick_random
